ac993d1db09d8d557a59fe20a39342b52345570f
David Smeringe <david@merea.se>
2013-04-16 11:12:37 +0200

    Added custom fields for page edit to functions.php

--- functions.php
+++ functions.php
@@ -170,4 +170,142 @@ function my_theme_add_editor_styles() {
 }
 add_action( 'init', 'my_theme_add_editor_styles' );
 
+
+
+/*********** CUSTOM FIELDS ***********************
+ * Examples adding custom fields to the post/page edit view
+ * See http://www.deluxeblogtips.com/2010/04/how-to-create-meta-box-wordpress-post.html
+ */
+/* //uncomment to use..  (all the way down to '//eo including bones custom fields')
+ 
+// prefix custom fields to prevent conflicts
+$prefix = 'bones_';
+$meta_box = array(
+	'id' => 'bones-meta-box',
+	'title' => 'Bones custom meta box',
+	'page' => 'post',
+	'context' => 'normal',
+	'priority' => 'high',
+	'fields' => array(
+		array(
+			'name' => 'Text box',
+			'desc' => 'Enter something here',
+			'id' => $prefix . 'text',
+			'type' => 'text',
+			'std' => 'Default value 1'
+		),
+		array(
+			'name' => 'Textarea',
+			'desc' => 'Enter big text here',
+			'id' => $prefix . 'textarea',
+			'type' => 'textarea',
+			'std' => 'Default value 2'
+		),
+		array(
+			'name' => 'Select box',
+			'id' => $prefix . 'select',
+			'type' => 'select',
+			'options' => array('Option 1', 'Option 2', 'Option 3')
+		),
+		array(
+			'name' => 'Radio',
+			'id' => $prefix . 'radio',
+			'type' => 'radio',
+			'options' => array(
+				array('name' => 'Name 1', 'value' => 'Value 1'),
+				array('name' => 'Name 2', 'value' => 'Value 2')
+			)
+		),
+		array(
+			'name' => 'Checkbox',
+			'id' => $prefix . 'checkbox',
+			'type' => 'checkbox'
+		)
+	)
+);
+
+//hook upp admin_menu and add our custom fields
+add_action('admin_menu', 'bones_add_box');
+
+// Add meta box
+function bones_add_box() {
+	global $meta_box;
+	add_meta_box($meta_box['id'], $meta_box['title'], 'bones_show_box', $meta_box['page'], $meta_box['context'], $meta_box['priority']);
+}
+
+
+// Callback function to show fields in meta box
+function bones_show_box() {
+	global $meta_box, $post;
+	// Use nonce for verification
+	echo '<input type="hidden" name="bones_meta_box_nonce" value="', wp_create_nonce(basename(__FILE__)), '" />';
+	echo '<table class="form-table">';
+	foreach ($meta_box['fields'] as $field) {
+		// get current post meta data
+		$meta = get_post_meta($post->ID, $field['id'], true);
+		echo '<tr>',
+		'<th style="width:20%"><label for="', $field['id'], '">', $field['name'], '</label></th>',
+		'<td>';
+		switch ($field['type']) {
+			case 'text':
+				echo '<input type="text" name="', $field['id'], '" id="', $field['id'], '" value="', $meta ? $meta : $field['std'], '" size="30" style="width:97%" />', '<br />', $field['desc'];
+				break;
+			case 'textarea':
+				echo '<textarea name="', $field['id'], '" id="', $field['id'], '" cols="60" rows="4" style="width:97%">', $meta ? $meta : $field['std'], '</textarea>', '<br />', $field['desc'];
+				break;
+			case 'select':
+				echo '<select name="', $field['id'], '" id="', $field['id'], '">';
+				foreach ($field['options'] as $option) {
+				echo '<option ', $meta == $option ? ' selected="selected"' : '', '>', $option, '</option>';
+				}
+				echo '</select>';
+				break;
+			case 'radio':
+				foreach ($field['options'] as $option) {
+					echo '<input type="radio" name="', $field['id'], '" value="', $option['value'], '"', $meta == $option['value'] ? ' checked="checked"' : '', ' />', $option['name'];
+				}
+				break;
+				case 'checkbox':
+				echo '<input type="checkbox" name="', $field['id'], '" id="', $field['id'], '"', $meta ? ' checked="checked"' : '', ' />';
+				break;
+		}
+		echo '</td><td>',
+		'</td></tr>';
+	}
+	echo '</table>';
+}
+
+//hook up the post save
+add_action('save_post', 'bones_save_data');
+// Save data from meta box
+function bones_save_data($post_id) {
+	global $meta_box;
+	// verify nonce
+	if (!wp_verify_nonce($_POST['bones_meta_box_nonce'], basename(__FILE__))) {
+		return $post_id;
+	}
+	// check autosave
+	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+		return $post_id;
+	}
+	// check permissions
+	if ('page' == $_POST['post_type']) {
+		if (!current_user_can('edit_page', $post_id)) {
+			return $post_id;
+		}
+	} elseif (!current_user_can('edit_post', $post_id)) {
+		return $post_id;
+	}
+	foreach ($meta_box['fields'] as $field) {
+		$old = get_post_meta($post_id, $field['id'], true);
+		$new = $_POST[$field['id']];
+		if ($new && $new != $old) {
+			update_post_meta($post_id, $field['id'], $new);
+		} elseif ('' == $new && $old) {
+			delete_post_meta($post_id, $field['id'], $old);
+		}
+	}
+}
+//------------ eo including bones custom fields -------------------
+********/
 ?>
